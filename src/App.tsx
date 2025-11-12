480260ac00b58bb27b488b5b19f42a6fe2da9b7c
Diego <dasc2703@gmail.com>
2025-11-12 11:31:04 -0500

    Max-width para pantallas grandes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Header, Main } from '@/components/layout'
 
 export default function App() {
   return (
-    <div className='px-3 md:px-12 lg:px-20 flex flex-col gap-10'>
+    <div className='px-3 md:px-12 lg:px-20 flex flex-col gap-10 max-w-[1440px] m-auto'>
       <Header/>
       <Main/>
     </div>
